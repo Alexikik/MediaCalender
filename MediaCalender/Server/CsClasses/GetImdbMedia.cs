95978753eae1afc4cf3a9c512e1f74ade8272960
Alexikik <Alexik1998@gmail.com>
2019-10-15 08:54:05 +0200

    Nyt forsøg med at bruge det nye API

--- MediaCalender/Server/CsClasses/GetImdbMedia.cs
+++ MediaCalender/Server/CsClasses/GetImdbMedia.cs
@@ -7,6 +7,10 @@ using MediaCalender.Shared.Containers;
 using System.Net;
 //using System.Web.Script.Serialization;
 using Nancy.Json;
+using System.Net.Http;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace MediaCalender.Server.CsClasses
 {
@@ -94,8 +98,8 @@ namespace MediaCalender.Server.CsClasses
             string url = $"https://api.thetvdb.com/search/series?name=South%20Park";
             using (WebClient webClient = new WebClient())
             {
+                webClient.Headers.Add("Authorization:-Bearer", "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzExNTcwNjgsImlkIjoiTWVkQ2FsIiwib3JpZ19pYXQiOjE1NzEwNzA2NjgsInVzZXJpZCI6NTQwNjMyLCJ1c2VybmFtZSI6IkFsZXhpazE5OThtaXUifQ.D240p8NzQL7xMiaHyAeF6PWeKVJ_IG987-r0amNuE1ix4NTPEHAsmvcnzRGyhsE6CMPJtU4Xi6Bs0QiX4mpqS5txEQCV7ZmIngV4hCpwUtxt2XOe7noBlPfSqkTHcvTGS5QlUqwn0GpZA1LxadkzpfHpbcrREZf-aJ472gXE_1iaOAlmUJZClVri9B_eTSPFeOYR5OtevW3BSXx0dSO5GfRDjtWTTpyrcfbFKYIYziKDK5kWtgt2gvuSgK2M2t58zfMZ_WwfNyDnoXm9gPxzve1M5hTSsUYHrjGpE7tUGLPCSNEFHMP_Z7yudpT7tPtoPjxhR9Ij6XowWIgI9RTn_A");
                 webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
-                webClient.Headers.Add("Authorization: Bearer", "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzExMzI2MTQsImlkIjoiTWVkQ2FsIiwib3JpZ19pYXQiOjE1NzEwNDYyMTQsInVzZXJpZCI6NTQwNjMyLCJ1c2VybmFtZSI6IkFsZXhpazE5OThtaXUifQ.j8irNUwn0s04_LuiGqBVA6ezYw9al_jM8ERfOJBZLhYhpBd6Vw-_1ZpkE56HQ1Z-JlUmaK_QpF8_zX-pNp4sl_tijGMtUupP8K_OWcbbnccG1bZo-_liAxDRqZePRFce-G91agzykuU2o1iuIB6RDRmtAljEHCVx7nRSCoaMvPBrPeaUZrCv2xuK8rkF217axnbndMYUZ9Ma0xmdfT1fqgF-fSFAkyJue2HbMj6zGPIgxZBSCjF789NKJ4bdzjKyo8BANTK19ynmFR4qwXP6e1qhm6dfJL538iz2lSkeEx0ddmR0xhXqaB7vEA0p7dAtK1zG0baE7RThiKZhAN8zUQ");
                 
                 var json = webClient.DownloadString(url);
                 //json.Replace("'", "´"); // Replaces "'" with "´"
@@ -104,9 +108,46 @@ namespace MediaCalender.Server.CsClasses
                 series = oJS.Deserialize<Series>(json);
             }
 
-
-
             return new Series();
         }
+
+        //Sets the token from the apikey from tvdb
+        private async Task SetToken(string ApiKey)
+        {
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                //Url to TVDB
+                Uri url = new Uri("https://api.thetvdb.com/login");
+
+                //Set Accept request header
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                //Setup request message with json apikey and content-type header
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+                request.Content = new StringContent("{\"apiKey\":\"" + ApiKey + "\"}", Encoding.UTF8, "application/json");
+
+                //Send via post, get response, read content into string, check to be sure it was OK
+                HttpResponseMessage resp = await client.SendAsync(request);
+                string respString = await resp.Content.ReadAsStringAsync();
+                if (resp.ReasonPhrase != "OK")
+                {
+                    throw new Exception(resp.ReasonPhrase);
+                }
+
+                //Read into byte array and them create a memory stream
+                byte[] byteArray = Encoding.UTF8.GetBytes(respString);
+                MemoryStream stream1 = new MemoryStream(byteArray);
+
+                //Create serializer and read the stream into a Token object
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(string));
+                _token = serializer.ReadObject(stream1) as Token;
+
+                //Set global token for debugging
+                tokenString = _token.token;
+            }
+        }
+
     }
 }
